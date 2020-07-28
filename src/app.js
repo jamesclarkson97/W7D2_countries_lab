4f43faf94ff7005c21c1ea48c58bc3f6c04a7e38
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-28 14:54:48 +0100

    lists all countries' data on screen

--- src/app.js
+++ src/app.js
@@ -4,13 +4,18 @@ document.addEventListener('DOMContentLoaded', ()=> {
     new Vue({
         el: '#app',
         data: {
-            country: null
+            countries: null
         },
+
+        mounted() {
+            this.fetchData();
+        },
+        
         methods: {
             fetchData(){
                 fetch("https://restcountries.eu/rest/v2/all")
                 .then(response => response.json())
-                .then(data => this.country = data[26].name);
+                .then(data => this.countries = data);
             }
         }
     });
